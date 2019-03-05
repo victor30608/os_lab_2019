54914a76e4d710fc19962d212fac6e7b7822fbaa
Exception <victor30608@yandex.ru>
2019-03-05 20:24:28 +0000

    изменём revert_string.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,17 @@
 
 void RevertString(char *str)
 {
-	// your code here
+int length = strlen(str);
+   char * temp = (char*)malloc((length + 1) * sizeof(char));
+   memcpy(temp, str, length + 1);
+   char * p_back = temp + length - 1;
+   
+   for (char * p = temp; p < p_back; p++, p_back--) {
+      *p ^= *p_back;
+      *p_back ^= *p;
+      *p ^= *p_back;
+   }
+   
+ memcpy(str, temp, length + 1);
 }
 
